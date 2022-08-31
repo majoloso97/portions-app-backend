a8a51e0349140769390050a0c125d637bba9a7bb
majoloso97 <majoloso97@gmail.com>
2022-08-31 12:40:32 -0600

    :rocket: Add railway config

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 from fastapi import FastAPI
 from events import start_handler
 from routers import portions
+import uvicorn
+import os
 
 app = FastAPI()
 
 app.add_event_handler('startup', start_handler(app))
 app.include_router(portions.router)
+
+if __name__ == '__main__':
+  uvicorn.run('main:app', host='0.0.0.0', port=os.getenv('PORT', default=8000), log_level='info')
